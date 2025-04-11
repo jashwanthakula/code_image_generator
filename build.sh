f214c5667d45faa8c1ba53828c7511e76da3c985
Jashwanth Akula <jashwanthakula.26@gmail.com>
2025-04-11 23:19:15 +0530

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -7,5 +7,9 @@ pip install -r requirements.txt
 # Install Playwright browsers
 playwright install
 
+#!/usr/bin/env bash
+echo "Installing Playwright browsers..."
+playwright install --with-deps
+
 # Optional: Verify installation
 playwright --version
